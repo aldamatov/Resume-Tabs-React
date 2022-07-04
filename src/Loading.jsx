eb6f11e703940f25e5e3932c5bb931a21fb99148
Erzhan Aldamatov <33819603+aldamatov@users.noreply.github.com>
2022-07-05 01:50:17 +1000

    Initial commit

---
+++ src/Loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>Loading</h1>
+      <img
+        className="loading-img"
+        src="https://i.imgur.com/llF5iyg.gif?noredirect"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Loading;
